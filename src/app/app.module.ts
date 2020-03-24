fc2e4244c3320c74b0c00fee295ee141a48d752d
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2020-03-24 10:55:52 -0700

    code cleanup with routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
-import { ComponentsComponent } from './shared/components/components.component';
-import { PipesComponent } from './shared/pipes/pipes.component';
-import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
@@ -15,10 +13,8 @@ import { UsersComponent } from './components/users/users.component';
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ComponentsComponent,
-    PipesComponent,
     DashboardComponent,
-    UsersComponent
+    UsersComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
-import { LogoutComponent } from './logout/logout.component';
 
 
 @NgModule({
-  declarations: [LoginComponent, LogoutComponent],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
     AuthRoutingModule
